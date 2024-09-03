1e3a906c1f74ec1da8641cba27f61c1d580d3df0
dmharrison <dmharrison2014@gmail.com>
2024-09-03 15:55:37 -0400

    began building the class PatientsInfo to perform analysis and the first method to analyze ages

--- medical _insurance_costs.py
+++ medical _insurance_costs.py
@@ -1,4 +1,6 @@
 import csv
+import os
+print("Current Working Directory:", os.getcwd())
 
 # In my Python file I create empty lists for the various attributes in insurance.csv
 ages = []
@@ -30,3 +32,33 @@ load_list_data(num_children, 'insurance.csv', 'children')
 load_list_data(smoker_statuses, 'insurance.csv', 'smoker')
 load_list_data(regions, 'insurance.csv', 'region')
 load_list_data(insurance_charges, 'insurance.csv', 'charges')
+
+class PatientsInfo:
+    # init method that takes in each list parameter
+    def __init__(self, patients_ages, patients_sexes, patients_bmis, patients_num_children, 
+                 patients_smoker_statuses, patients_regions, patients_charges):
+        self.patients_ages = patients_ages
+        self.patients_sexes = patients_sexes
+        self.patients_bmis = patients_bmis
+        self.patients_num_children = patients_num_children
+        self.patients_smoker_statuses = patients_smoker_statuses
+        self.patients_regions = patients_regions
+        self.patients_charges = patients_charges
+
+      # method that calculates the average ages of the patients in insurance.csv
+    def analyze_ages(self):
+        # initialize total age at zero
+        total_age = 0
+        # iterate through all ages in the ages list
+        for age in self.patients_ages:
+            # sum of the total age
+            total_age += int(age)
+
+        # Calculate the average age
+        average_age = round(total_age/len(self.patients_ages), 2)
+
+        # return total age divided by the length of the patient list
+        return (f"Average Patient Age: {average_age} years")
+    
+    
+
